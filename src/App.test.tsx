faf5ca7672fb451b354757d247d38001659db42b
PelleLindroth <pellelindroth@gmail.com>
2021-12-15 11:25:42 +0100

    Add MemoryRouter to render App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  )
+
+  expect(1 + 1).toBe(2)
+})
+
+// Home view
+// shows meetup details on separate page when clicking arrow link
+// renders existing comments and reviews on past events on detail page
