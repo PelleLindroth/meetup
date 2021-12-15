3a198a8b305fbe2fc463eb7039321f50a790b960
PelleLindroth <pellelindroth@gmail.com>
2021-12-15 11:28:04 +0100

    Add meetups state and route to single meetup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import { Meetup } from './db/meetups'
+import { getAllMeetups } from './db'
 import Home from './views/Home'
+import SingleMeetup from './views/SingleMeetup'
 
 function App() {
+  const [meetups, setMeetups] = useState<Meetup[]>([])
+
+  useEffect(() => {
+    const dbMeetups = getAllMeetups()
+
+    setMeetups(dbMeetups)
+  }, [])
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={<Home meetups={meetups} setMeetups={setMeetups} />}
+      />
+      <Route
+        path="/meetup/:id"
+        element={<SingleMeetup setMeetups={setMeetups} />}
+      />
     </Routes>
   )
 }
