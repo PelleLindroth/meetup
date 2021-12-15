754fad0ed6081946e320e224f359d1767cf3dcf4
PelleLindroth <pellelindroth@gmail.com>
2021-12-15 19:59:44 +0100

    Make Header work w/ logged out and logged in user

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,60 @@
 import { Link } from 'react-router-dom'
-import headerStyles from './Header.module.scss'
 import Logo from '../../assets/logo.png'
 import { useLocation } from 'react-router'
-const Header = () => {
+import { User } from '../../db/users'
+import UserIcon from '../../assets/icons/user.png'
+import { useState } from 'react'
+import styles from './Header.module.scss'
+
+type HeaderProps = {
+  user: User | null
+  setUser: React.Dispatch<React.SetStateAction<User | null>>
+}
+
+const Header = (props: HeaderProps) => {
+  const { user, setUser } = props
   const location = useLocation()
+  const [showProfileMenu, setShowProfileMenu] = useState<boolean>(false)
+
+  const handleLogout = () => {
+    setUser(null)
+  }
 
   return (
-    <header className={headerStyles.header}>
-      <Link to="/">
-        <img src={Logo} alt="logo" />
-      </Link>
-      {location.pathname !== '/login' && (
-        <Link to="/login">
-          <button className={headerStyles.loginButton}>Log in</button>
+    <header className={styles.header}>
+      <div className={styles.innerContainer}>
+        <Link to="/">
+          <img src={Logo} alt="logo" />
         </Link>
-      )}
+        {location.pathname !== '/login' && !user && (
+          <Link to="/login">
+            <button className={styles.loginButton}>Log in</button>
+          </Link>
+        )}
+        {user && location.pathname !== `/profile/${user.id}` && (
+          <div
+            className={styles.profileWrapper}
+            title="Profile menu"
+            onClick={() => setShowProfileMenu(!showProfileMenu)}
+          >
+            <img src={UserIcon} alt="User profile icon" />
+            <p>{`${user.firstName} ${user.lastName}`}</p>
+            {showProfileMenu && (
+              <nav className={styles.profileMenu}>
+                <Link
+                  className={styles.viewProfileButton}
+                  to={`/profile/${user!.id}`}
+                >
+                  View profile
+                </Link>
+                <button onClick={handleLogout} className={styles.logoutButton}>
+                  Log out
+                </button>
+              </nav>
+            )}
+          </div>
+        )}
+      </div>
     </header>
   )
 }
