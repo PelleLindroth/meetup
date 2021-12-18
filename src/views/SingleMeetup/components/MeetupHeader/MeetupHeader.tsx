e4c7a1f21f1991a499557abd75b94507069bdbae
PelleLindroth <pellelindroth@gmail.com>
2021-12-18 11:38:53 +0100

    Implement signup and cancel event functionality

---
+++ src/views/SingleMeetup/components/MeetupHeader/MeetupHeader.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { signUpForEvent, cancelSignUpForEvent } from '../../../../db'
+import { MeetupHeaderProps } from '../../types'
+import styles from '../../SingleMeetup.module.scss'
+
+const MeetupHeader = (props: MeetupHeaderProps) => {
+  const { meetup, user, isUpcomingEvent } = props
+  const [attending, setAttending] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (user) {
+      setAttending(user.attending.includes(meetup.id))
+    }
+  }, [meetup.id, user])
+
+  const handleSignUp = () => {
+    signUpForEvent(meetup, user!)
+    setAttending(true)
+  }
+
+  const handleCancel = () => {
+    cancelSignUpForEvent(meetup, user!)
+    setAttending(false)
+  }
+
+  return (
+    <section>
+      <h1
+        className={isUpcomingEvent ? styles.upcomingHeader : styles.pastHeader}
+      >
+        {meetup.title}
+      </h1>
+      <h2>{meetup.description}</h2>
+      {isUpcomingEvent && user && !attending && (
+        <button onClick={handleSignUp} className={styles.signUpButton}>
+          Sign up for this event
+        </button>
+      )}
+      {isUpcomingEvent && user && attending && (
+        <div className={styles.signedUpMessage}>
+          <p>You have signed up for this event!</p>
+          <button onClick={handleCancel}>Cancel</button>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default MeetupHeader
