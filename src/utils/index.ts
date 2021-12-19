6acc2ed571f02fa62a200248cd1751c33bdee2a7
PelleLindroth <pellelindroth@gmail.com>
2021-12-20 00:18:28 +0100

    Add functions for user meetup lists

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+import { User } from '../db/users'
+import { Meetup } from '../db/meetups'
+
 export const formatDate = (date: Date) => {
   return `${new Intl.DateTimeFormat('en-GB', {
     year: 'numeric',
@@ -6,4 +9,41 @@ export const formatDate = (date: Date) => {
     hour: 'numeric',
     minute: 'numeric',
   }).format(date)}`
+}
+
+const sortChronologically = (array: Meetup[]) => {
+  return array.sort((a, b) => a.date.getTime() - b.date.getTime())
+}
+
+export const createUserLists = (user: User, meetups: Meetup[]) => {
+  return {
+    ownUpcoming: sortChronologically(
+      meetups.filter(
+        (meetup: Meetup) =>
+          meetup.arranger.id === user!.id &&
+          meetup.date.getTime() > new Date().getTime()
+      )
+    ),
+    ownPast: sortChronologically(
+      meetups.filter(
+        (meetup: Meetup) =>
+          meetup.arranger.id === user!.id &&
+          meetup.date.getTime() < new Date().getTime()
+      )
+    ),
+    upcomingAttending: sortChronologically(
+      meetups.filter(
+        (meetup: Meetup) =>
+          user!.attending.includes(meetup.id) &&
+          meetup.date.getTime() > new Date().getTime()
+      )
+    ),
+    pastAttended: sortChronologically(
+      meetups.filter(
+        (meetup: Meetup) =>
+          user!.attending.includes(meetup.id) &&
+          meetup.date.getTime() < new Date().getTime()
+      )
+    ),
+  }
 }
