8a9fc3399240cf8379c8574d4d342b69143745ec
PelleLindroth <pellelindroth@gmail.com>
2021-12-20 00:21:48 +0100

    Add data testid for users meetups

--- src/views/Home/components/MeetupList/MeetupList.tsx
+++ src/views/Home/components/MeetupList/MeetupList.tsx
@@ -3,11 +3,19 @@ import styles from './MeetupList.module.scss'
 import { MeetupListProps } from './types'
 
 const MeetupList = (props: MeetupListProps) => {
-  const { meetups, upcoming } = props
+  const { meetups, upcoming, own } = props
 
   return (
     <ul
-      data-testid={upcoming ? 'upcoming-events' : 'past-events'}
+      data-testid={
+        own
+          ? upcoming
+            ? 'own-upcoming-events'
+            : 'own-past-events'
+          : upcoming
+          ? 'upcoming-events'
+          : 'past-events'
+      }
       className={styles.meetupList}
     >
       {meetups.map((meetup) => (
--- src/views/Home/components/MeetupList/types.ts
+++ src/views/Home/components/MeetupList/types.ts
@@ -3,4 +3,5 @@ import { Meetup } from '../../../../db/meetups'
 export type MeetupListProps = {
   meetups: Meetup[]
   upcoming?: boolean
+  own?: boolean
 }
