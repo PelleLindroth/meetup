ed58dade9cc7f4326c697ce148ab53904753b065
PelleLindroth <pellelindroth@gmail.com>
2021-12-20 00:20:58 +0100

    Update CreateMeetup props type

--- src/views/CreateMeetup/types.ts
+++ src/views/CreateMeetup/types.ts
@@ -1,5 +1,7 @@
 import { User } from '../../db/users'
+import { Meetup } from '../../db/meetups'
 
 export type CreateMeetupProps = {
   user: User
+  setMeetups: React.Dispatch<React.SetStateAction<Meetup[]>>
 }
