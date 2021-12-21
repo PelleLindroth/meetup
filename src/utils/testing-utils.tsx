3743df534cbcffb1251af04f46f681251f1a177b
PelleLindroth <pellelindroth@gmail.com>
2021-12-21 20:44:18 +0100

    Add context wrapper to testing utils

--- src/utils/testing-utils.tsx
+++ src/utils/testing-utils.tsx
@@ -1,26 +1,40 @@
+import { mount } from 'enzyme'
 import { render } from '@testing-library/react'
 import { ReactElement } from 'react'
 import { MemoryRouter } from 'react-router-dom'
+import { DateContext, DateContextInterface } from '../contexts/DateContext'
 import { Routes, Route } from 'react-router'
-import { mount } from 'enzyme'
 
-export const renderWithRouter = (ui: ReactElement, options?: any) =>
-  render(ui, { wrapper: MemoryRouter, ...options })
+const mockDateContext: DateContextInterface = {
+  customDate: new Date(1640354400000),
+  setCustomDate: jest.fn(),
+}
+
+export const renderWithRouter = (ui: ReactElement) =>
+  render(
+    <DateContext.Provider value={mockDateContext}>
+      <MemoryRouter>{ui}</MemoryRouter>
+    </DateContext.Provider>
+  )
 
 export const renderWithPath = (ui: ReactElement, entry: string, path: string) =>
   render(
-    <MemoryRouter initialEntries={[entry]}>
-      <Routes>
-        <Route path={path} element={ui} />
-      </Routes>
-    </MemoryRouter>
+    <DateContext.Provider value={mockDateContext}>
+      <MemoryRouter initialEntries={[entry]}>
+        <Routes>
+          <Route path={path} element={ui} />
+        </Routes>
+      </MemoryRouter>
+    </DateContext.Provider>
   )
 
 export const mountWithPath = (ui: ReactElement, entry: string, path: string) =>
   mount(
-    <MemoryRouter initialEntries={[entry]}>
-      <Routes>
-        <Route path={path} element={ui} />
-      </Routes>
-    </MemoryRouter>
+    <DateContext.Provider value={mockDateContext}>
+      <MemoryRouter initialEntries={[entry]}>
+        <Routes>
+          <Route path={path} element={ui} />
+        </Routes>
+      </MemoryRouter>
+    </DateContext.Provider>
   )
